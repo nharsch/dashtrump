bdebd6f6b392e67febec4f18f455b5b114b9414f
Nigel Harsch <nigelharsch@gmail.com>
2017-02-15 22:47:02 -0500

    simple templates

--- src/dashtrump/core.clj
+++ src/dashtrump/core.clj
@@ -14,19 +14,32 @@
     (Integer. (re-find #"[0-9]+" (first
         (:content (first (html/select dom trump-selector))))))))
 
+(html/deftemplate base "dashtrump/templates/base.html"
+  [{:keys [title main]}]
+  [:title] (html/content title)
+  [:body]  (html/content main))
+
+(html/deftemplate yup "dashtrump/templates/yup.html"
+  [rating]
+  [:span#rating] (html/content (str rating)))
+
+(html/deftemplate nope "dashtrump/templates/yup.html"
+  [rating]
+  [:span#rating] (html/content (str rating)))
 
 (defn get-template [rating]
   (if (< rating 50)
-    (str "Yup. Trump's approval rating is down to " rating "%")
-    (str "Dear God, something is wrong. "
-         rating "% of Americans approve of DT")))
+    (yup rating)
+    (nope rating)
+    ;; (str "Yup. Trump's approval rating is down to " rating "%")
+    ;; (str "Dear God, something is wrong. "
+    ;;      rating "% of Americans approve of DT")
+    )
+)
 
 (defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body (get-template
            (find-rating-in-dom
-             (html/html-snippet (:body @(http/get appr-url))))
-           )
-  }
-)
+             (html/html-snippet (:body @(http/get appr-url)))))})
