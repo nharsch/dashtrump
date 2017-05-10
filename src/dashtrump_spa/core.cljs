a711ea9dbd509ad15dd139308494cc24d3a17396
Nigel Harsch <nigelharsch@gmail.com>
2017-05-10 01:08:50 -0400

    api works

--- src/dashtrump_spa/core.cljs
+++ src/dashtrump_spa/core.cljs
@@ -1,17 +1,12 @@
 (ns dashtrump_spa.core
   (:require 
     [ajax.core :refer [GET]]
+    [reagent.core :as reagent]
     ))
 
 (enable-console-print!)
 
 (println "This text is printed from src/dashtrump_spa/core.cljs. Go ahead and edit it and see reloading in action.")
-
-;; define your app data so that it doesn't get over-written on reload
-
-(def api-url "http://doesamericastillhatetrump.com/rating")
-
-(defonce app-state (atom {:text "Hello world!"}))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
@@ -19,25 +14,40 @@
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
 )
 
-;; scraper
+;; define your app data so that it doesn't get over-written on reload
 
-;; (defn find-rating-in-dom [dom]
-;;   "fetch rating number from full dom"
-;;   ;; (hick/parse dom)
-;; )
+(def api-url "http://dashtrump.herokuapp.com/rating")
 
-;; ( (
-;;        -    [trump-selector
-;;              -     [:div#rbnSection323SECTION-526 :> :table :> :tbody :>
-;;                     -       [:tr  (html/nth-of-type 6)] :> :td.rbnsctPoint]]
-;;        -    (Integer.  (re-find #"[0-9]+"  (first
-;;                                              -        (:content  (first  (html/select dom trump-selector))))))))
+(defonce app-state
+  (reagent/atom {:rating "not set"
+                 :fetched-state "NOT FETCHED"
+                 }))
 
+(defn set-getting-rating []
+  "set state to fetching"
+  (swap! app-state assoc :fetched-state "FETCHING"))
+
+(defn set-got-rating [rating]
+  "update rating"
+  (reset! app-state {:rating rating :fetched-state "FETCHED"}))
+
+;; (defn page-load)
 
 (defn get-rating []
   "get approval page, return HTML sting"
-  (println "calling")
-  (GET api-url {:handler #(println (str %))})
-  )
+  ;; (println "calling")
+  (set-getting-rating)
+  (GET api-url {:handler
+                #(set-got-rating (get % "rating"))
+                }))
 
-(get-rating)
+(defn message []
+  [:div
+   [:h1 "rating is " (:rating @app-state)]
+   [:p "app state is " (:fetched-state @app-state)]
+   [:button {:onClick #(get-rating)} "get rating"]
+   ])
+
+
+(reagent/render [message] (js/document.querySelector "#app"))
+(get-rating) 
